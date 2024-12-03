14d0061b12021dad875f18caa285b995d06d0eaf
Xyn <me@xyn.ro>
2024-12-03 12:43:30 +0100

    "Configure logging to use a RotatingFileHandler instead of just printing to console

--- pypga/__init__.py
+++ pypga/__init__.py
@@ -3,3 +3,14 @@ PYthon Programmable Gate Array
 """
 from . import boards, core, modules
 from .core import interface
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Basic configuration for logging
+logging.basicConfig(
+    handlers=[RotatingFileHandler('./pypga_warning.log', maxBytes=100000, backupCount=5)],
+    level=logging.WARNING,  # Set the logging level to WARNING
+    format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s() - %(message)s'  # Custom format
+)
+print([k for k in logging.Logger.manager.loggerDict])
