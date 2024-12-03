220d6fd71c768ef8153f78fc9766ba71b5610f11
Xyn <me@xyn.ro>
2024-12-03 12:48:47 +0100

    Remove forgotten print

--- pypga/__init__.py
+++ pypga/__init__.py
@@ -7,10 +7,8 @@ from .core import interface
 import logging
 from logging.handlers import RotatingFileHandler
 
-# Basic configuration for logging
 logging.basicConfig(
     handlers=[RotatingFileHandler('./pypga_warning.log', maxBytes=100000, backupCount=5)],
-    level=logging.WARNING,  # Set the logging level to WARNING
-    format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s() - %(message)s'  # Custom format
+    level=logging.WARNING,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s() - %(message)s'
 )
-print([k for k in logging.Logger.manager.loggerDict])
