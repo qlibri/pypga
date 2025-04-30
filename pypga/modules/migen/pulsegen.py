62b93149459dfb2e9b4cc07c3bb9fd14e5950f94
josselex <8465194+josselex@users.noreply.github.com>
2025-04-30 14:34:51 +0200

    try again

--- pypga/modules/migen/pulsegen.py
+++ pypga/modules/migen/pulsegen.py
@@ -48,15 +48,15 @@ class MigenPulseGen(MigenModule):
         self.sync += [
             self.out.eq(self.carry & on),
             If(
-                on == 0,  # prepare for the first pulse when off
+                on == 0 | self.restart,  # prepare for the first pulse when off
                 self.carry.eq(high_after_on),
                 self.count.eq(period - first_cycle_period_offset),
+                If(self.restart, self.restart.eq(0))
             )
             .Elif(
-                self.carry | self.restart,  # restart countdown
+                self.carry,  # restart countdown
                 self.carry.eq(0),
-                self.count.eq(period),
-                If(self.restart, self.restart.eq(0))
+                self.count.eq(period)
             )
             .Else(  # regular countdown
                 Cat(self.count, self.carry).eq(Cat(self.count, 0) - 1),
